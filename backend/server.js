13fe16ae2a1a86d813fa9c3c8fe778af351d5ec0
alejolagosm <falame94@hotmail.com>
2022-03-01 22:31:32 -0500

    Added the authorization backedn functionality and started developing the front end

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('server...');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+// Connect to the database
+connectDB();
+// start server using express
+const app = express();
+
+// This allows the API to get data from the body of the request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the support tickets API' });
+});
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+## Front End for the ticket support app
